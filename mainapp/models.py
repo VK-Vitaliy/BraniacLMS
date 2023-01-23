c091a1a2506606496ff5ae52fb552b94be395be1
kv <kulvitall.vk@gmail.com>
2023-01-23 16:42:14 +0300

    fixtures

--- mainapp/models.py
+++ mainapp/models.py
@@ -23,6 +23,7 @@ class Courses(models.Model):
     description = models.TextField(verbose_name="Description", blank=True, null=True)
     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
     cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
     updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
     deleted = models.BooleanField(default=False)
@@ -35,18 +36,18 @@ class Courses(models.Model):
         self.save()
 
 
-class Lessons(models.Model):
-    courses = models.ForeignKey(Courses, on_delete=models.CASCADE)
+class Lesson(models.Model):
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     num = models.PositiveIntegerField(verbose_name="Lesson number")
     title = models.CharField(max_length=256, verbose_name="Name")
     description = models.TextField(verbose_name="Description", blank=True, null=True)
     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
-        return f"{self.courses.name} | {self.num} | {self.title}"
+        return f"{self.course.name} | {self.num} | {self.title}"
 
     def delete(self, *args):
         self.deleted = True
